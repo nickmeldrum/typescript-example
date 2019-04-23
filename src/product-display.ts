a5c8a40890d8217b1e7e9f6b6752127db467cc59
Nick Meldrum <nick@nickmeldrum.com>
2019-04-23 13:14:48 +0100

    add prettier

--- src/product-display.ts
+++ src/product-display.ts
@@ -1,15 +1,12 @@
 interface Product {
-  name: string;
-  brand: string;
-  price: number;
+  name: string
+  brand: string
+  price: number
 }
 
-const display = (product: Product): string => `Buy ${product.brand}'s ${product.name} for £${product.price}!`
+const display = (product: Product): string =>
+  `Buy ${product.brand}'s ${product.name} for £${product.price}!`
 
-const product = {
-  name: 'Fursty Ferret',
-  brand: 'Badger',
-  price: 1.95
-}
+const product = { name: 'Fursty Ferret', brand: 'Badger', price: 1.95 }
 
 export default display(product)
